6bb298cfa6a3d3404f0bca815165a9be625dcfd8
Oliver Heer <oliver.heer@hotmail.com>
2022-01-01 09:59:31 +0100

    Add PipelineRunner and PipelineUtil

---
+++ src/main/java/ch/mscwi/wikidata/pipeline/PipelineRunner.java
@@ -0,0 +1,29 @@
+package ch.mscwi.wikidata.pipeline;
+
+import org.apache.beam.runners.direct.DirectRunner;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.PCollection;
+
+import ch.mscwi.wikidata.pojo.Activity;
+import ch.mscwi.wikidata.util.PipelineUtil;
+
+public class PipelineRunner {
+
+	public static void main(String[] args) throws Exception {
+		PipelineOptions options = PipelineOptionsFactory.create();
+		options.setRunner(DirectRunner.class);
+		options.setJobName("Opernhaus_Zürich_Pipeline");
+		
+		Pipeline pipeline = Pipeline.create(options);
+		
+    	PCollection<Activity> data = XmlProcurer.procure(pipeline, "https://www.opernhaus.ch/xmlexport/kzexport.xml");
+		data.apply(PipelineUtil.printActivities());
+    	
+    	PipelineResult result = pipeline.run();
+    	System.out.println(result.getState());
+    }
+
+}
---
+++ src/main/java/ch/mscwi/wikidata/util/PipelineUtil.java
@@ -0,0 +1,20 @@
+package ch.mscwi.wikidata.util;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.ParDo.SingleOutput;
+
+import ch.mscwi.wikidata.pojo.Activity;
+
+public class PipelineUtil {
+	
+	public static SingleOutput<Activity, Activity> printActivities() {
+		return ParDo.of(new DoFn<Activity, Activity>() {
+			@ProcessElement
+			public void processElement(ProcessContext context) {
+				System.out.println(context.element());
+			}
+		});
+	}
+
+}
