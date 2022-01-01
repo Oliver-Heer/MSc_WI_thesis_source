67a688cf0ee1307c84690e82e6b5e67ed8618888
Oliver Heer <oliver.heer@hotmail.com>
2022-01-01 10:59:16 +0100

    Add SpringBoot

--- src/main/java/ch/mscwi/wikidata/pipeline/PipelineRunner.java
+++ src/main/java/ch/mscwi/wikidata/pipeline/PipelineRunner.java
@@ -6,13 +6,18 @@ import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.values.PCollection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import ch.mscwi.wikidata.pojo.Activity;
 import ch.mscwi.wikidata.util.PipelineUtil;
 
+@SpringBootApplication
 public class PipelineRunner {
 
 	public static void main(String[] args) throws Exception {
+		SpringApplication.run(PipelineRunner.class, args);
+		
 		PipelineOptions options = PipelineOptionsFactory.create();
 		options.setRunner(DirectRunner.class);
 		options.setJobName("Opernhaus_Zürich_Pipeline");
